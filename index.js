f5eea8a9ae58561bd6c7ce7f3e7aced979cf8ded
Mauricio Andrades <mod.and.op@gmail.com>
2017-06-12 12:02:16 -0700

    added comments

--- index.js
+++ index.js
@@ -9,9 +9,9 @@
             curr_scope.forEach(function(e, i) {
                 scope[e] = scope[e] || [];
                 scope[e].push(curr_extension_number);
-            })
+            });
         }
-    })
+    });
 
     function parseHTML(str) {
         var tmp = document.implementation.createHTMLDocument();
@@ -31,21 +31,23 @@
             }
             buildContent() {
                 function create_elem(type, value) {
+                    // here we can add custom css to each element in the mapping container.
+                    // format should be camelCalse. paddingLeft will be added as padding-left.
                     var css = {
                         div: {
                             display: 'inline-block'
                         },
                         loadrule: {
-                            "paddingLeft": '3px',
-                            "display": "inline-block",
-                            "maxWidth": '100px',
-                            "height": "14px"
+                            'paddingLeft': '3px',
+                            'display': 'inline-block',
+                            'maxWidth': '100px',
+                            'height': '14px'
                         },
                         extension: {
-                            "paddingLeft": '3px',
-                            "display:": "inline-block",
-                            "maxWidth": '100px',
-                            "height": "14px"
+                            'paddingLeft': '3px',
+                            'display:': 'inline-block',
+                            'maxWidth': '100px',
+                            'height': '14px'
                         }
                     };
                     var element;
@@ -60,14 +62,13 @@
                         for (var key in css[type]) element.style[key] = css[type][key];
                             return element;
                     }
-                    if (type === "extension") {
+                    if (type === 'extension') {
                         element = parseHTML(value.join(''))[0];
                         for (var key in css[type]) element.style[key] = css[type][key];
                             return element;
                     }
                 }
                 var div = create_elem('div');
-                debugger;
                 if (this.lr) {
                     div.appendChild(create_elem('loadrule', this.lr));
                 }
@@ -76,34 +77,34 @@
                 }
                 return div;
             }
-        }
+        };
         var mapping_container = new MappingContainer(elem.dataset.extensions, elem.dataset.loadRules);
         var anchor = elem.children[0].children[1].children[5];
-        anchor.parentNode.insertBefore(mapping_container.buildContent(), anchor)
+        anchor.parentNode.insertBefore(mapping_container.buildContent(), anchor);
     }
     // build the data-set
-    var tag = (function() {
+    var tags = (function() {
         return {
             elems: jQuery('.manage_container').toArray(),
             scope: scope
         };
     }());
     // add data-set to dom elements
-    tag.elems.map(function(elem) {
+    tags.elems.map(function(elem) {
         elem.dataset.loadRules = utui.data.manage[elem.dataset.id].loadrule;
-        if (tag.scope[elem.dataset.id]) {
-            elem.dataset.extensions = tag.scope[elem.dataset.id].join(', ');
+        if (tags.scope[elem.dataset.id]) {
+            elem.dataset.extensions = tags.scope[elem.dataset.id].join(', ');
         } else {
-            elem.dataset.extensions = "";
+            elem.dataset.extensions = '';
         }
         return elem;
     });
-    // build mapping container elems
-    tag.elems.forEach(function(elem) {
+    // Here we build the mapping container for each tag in the UI.
+    tags.elems.forEach(function(elem) {
         buildMappingContainer(elem);
-    })
+    });
 })();
 
 var remove_elems = function() {
-    return document.querySelectorAll('.container_label.expand').forEach(function(o){console.log(o.parentNode.removeChild(o))})
+    return document.querySelectorAll('.container_label.expand').forEach(function(o) {console.log(o.parentNode.removeChild(o))});
 }
